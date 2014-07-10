e89bcb1f5892c4434b3dcf885563f6509275dca2
AJ Christensen <aj@junglist.io>
2014-07-10 12:12:03 +1200

    keywords updates

--- handlers/keywords.go
+++ handlers/keywords.go
@@ -1,10 +1,11 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/danryan/hal"
 	"github.com/davecgh/go-spew/spew"
 )
+
+const okStr = "OK"
 
 // GetHandler pulls a key out of the store, when addressed like so:
 // foobar?
@@ -16,7 +17,7 @@ var GetHandler = hal.Hear(`^(.+)?$`, func(res *hal.Response) error {
 		res.Send(err.Error())
 		return err
 	}
-	return res.Send(fmt.Sprintf("get: %s=%s", key, string(val)))
+	return res.Send(string(val))
 })
 
 // SetHandler does a simple match/set into the Store
@@ -28,7 +29,7 @@ var SetHandler = hal.Hear(`^(.+) is (.+)$`, func(res *hal.Response) error {
 		res.Send(err.Error())
 		return err
 	}
-	return res.Send(fmt.Sprintf("set: %s=%s", key, val))
+	return res.Send(okStr)
 })
 
 // DeleteHandler nukes keys in the Store
@@ -39,11 +40,22 @@ var DeleteHandler = hal.Hear(`^forget (.+)$`, func(res *hal.Response) error {
 		res.Send(err.Error())
 		return err
 	}
-	return res.Send(fmt.Sprintf("delete: %s", key))
+	return res.Send(okStr)
 })
 
 // UsersHandler provides insite into the Robots user storage
 var UsersHandler = hal.Hear(`^!show users$`, func(res *hal.Response) error {
-	line := spew.Sdump("%#v\n", res.Robot.Users)
+	lines := []string{}
+	for _, user := range res.Robot.Users.All() {
+		lines = append(lines, spew.Sdump(user))
+	}
+	return res.Send(lines...)
+})
+
+// UserHandler is the singular form of UsersHandler
+var UserHandler = hal.Hear(`^!show user (.+)$`, func(res *hal.Response) error {
+	id := res.Match[0][1]
+	user, _ := res.Robot.Users.Get(id)
+	line := spew.Sdump(user)
 	return res.Send(line)
 })
